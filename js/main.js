c1861fc504c80b0d28016d5d3fb986187bbd7ca9
sylviana <51912585+orcade@users.noreply.github.com>
2020-06-03 04:23:15 +0200

    maj
    test

--- js/main.js
+++ js/main.js
@@ -9,9 +9,13 @@ menuMainDom = document.getElementById('menu-principale');
 if (menuHamDom != null) {
     menuHamDom.addEventListener('click', function (e) {
         menuHamDom.classList.toggle("is-open");
+
         menuMainDom.classList.toggle("is-visible");
         e.preventDefault();
     });
 
 
 }
+
+
+
