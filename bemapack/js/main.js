bb5b99158ec43de885ea6c93f0732d32be177942
sylviana <51912585+orcade@users.noreply.github.com>
2020-07-15 19:51:41 +0200

    header

---
+++ bemapack/js/main.js
@@ -0,0 +1,49 @@
+"use strict";
+// variables
+var menuHamDom;
+var menuMainDom;
+
+var emailApi;
+
+menuHamDom = document.getElementById('menu-hamburger');
+menuMainDom = document.getElementById('menu-principale');
+
+if (menuHamDom != null) {
+    menuHamDom.addEventListener('click', function (e) {
+        menuHamDom.classList.toggle("is-open");
+        menuMainDom.classList.toggle("is-visible");
+        e.preventDefault();
+    });
+
+    menuMainDom.addEventListener('click', function (e) {
+        menuHamDom.classList.toggle("is-open");
+        menuMainDom.classList.toggle("is-visible");
+    });
+
+}
+
+
+
+// Initial visibility set to "none"
+
+ 
+const arrowUp = document.getElementById("menu-up");
+ 
+arrowUp.style.display = "none";
+ 
+ 
+ 
+// Visibility  "background" when scrolling
+ 
+window.addEventListener("scroll", function(){
+ 
+	if(window.pageYOffset >  10){
+ 
+		arrowUp.style.display = "block";
+	}
+	else{
+ 
+		arrowUp.style.display = "none";
+	}
+}, false);
+
